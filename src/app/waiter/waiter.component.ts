be3a25a07e51e213d886cc09ea2304527d4c3d36
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-01-21 21:53:18 +0100

    Added Waiter Method (without changes on the server)

--- src/app/waiter/waiter.component.ts
+++ src/app/waiter/waiter.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../shared/localStorage.service';
+
+const baseURL = "http://localhost:3000";
 
 @Component({
   selector: 'app-waiter',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaiterComponent implements OnInit {
 
-  constructor() { }
+  called: boolean = false;
+
+  constructor(private localStorageServie: LocalStorageService ,private http: HttpClient) { 
+  }
 
   ngOnInit() {
+    if(this.localStorageServie.getData != null){
+      var temp = this.localStorageServie.getData();
+      if(temp != null){
+        if(temp.match('true')){
+          this.called = true;
+        }else{
+        this.called = false;
+      }
+    }  
+  }
+}
+
+  callWaiter(){
+    this.called = true;
+    this.localStorageServie.setInfo(this.called);
+    let body: number = 1;
+    this.http.post(baseURL + "1" + "/callWaiter", body);
   }
 
 }
