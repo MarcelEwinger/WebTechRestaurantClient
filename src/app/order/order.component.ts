582140d4362742bce5df8cf2b233f549e7481ed8
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-01-22 22:19:29 +0100

    aenderungen und so

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 var placedOrder: boolean = false;
 
@@ -17,8 +18,10 @@ export class OrderComponent implements OnInit {
 
   showOrder: boolean = false;
   status: String = "";
+  orderID: number = 0;
   
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.orderID = 0;
     this.status = this.getOrderStatus();
 
   }
@@ -30,14 +33,14 @@ export class OrderComponent implements OnInit {
   
   }
 
+
   getOrderStatus(): String{
-
-
-
-
 
     return "";
   }
 
+  getStatus(): Observable<any>{
+    return this.http.get<String>(baseURL + "/" + table + "/getOrderStatus/" + this.orderID);
+  }
 
 }
