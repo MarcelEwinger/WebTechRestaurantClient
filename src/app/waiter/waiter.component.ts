3a99e9f18b0fa097806e2c9eff919aaa8496b37f
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-01-22 11:28:10 +0100

    Correction in Waiter Method
    Corrected the Methods, so we can get the current Status of the waiter call from the backend

--- src/app/waiter/waiter.component.ts
+++ src/app/waiter/waiter.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from '../shared/localStorage.service';
+import { Observable } from 'rxjs';
+//import { LocalStorageService } from '../shared/localStorage.service';
 
 const baseURL = "http://localhost:3000";
 
@@ -12,28 +13,44 @@ const baseURL = "http://localhost:3000";
 export class WaiterComponent implements OnInit {
 
   called: boolean = false;
+  date: Date = new Date();
+  timestamp: String = "";
+  table: number = 1;
+  callId: number = -1;
+  status: String = "";
 
-  constructor(private localStorageServie: LocalStorageService ,private http: HttpClient) { 
+  constructor(private http: HttpClient) { 
+    
   }
 
-  ngOnInit() {
-    if(this.localStorageServie.getData != null){
-      var temp = this.localStorageServie.getData();
-      if(temp != null){
-        if(temp.match('true')){
-          this.called = true;
-        }else{
-        this.called = false;
-      }
-    }  
+  ngOnInit(){
+
+    if(this.callId != -1){
+
+    this.getStatus().subscribe((data: String) => this.status = data);
+    if(this.status == 'waiting'){
+      this.called = true;
+      
+    }else{
+      this.called = false;
+      this.timestamp = "";
+      this.callId = -1;
+    }
   }
 }
 
   callWaiter(){
     this.called = true;
-    this.localStorageServie.setInfo(this.called);
-    let body: number = 1;
-    this.http.post(baseURL + "1" + "/callWaiter", body);
+    this.timestamp = this.date.getFullYear() + "-" + this.date.getMonth() + "-" + this.date.getDay();
+    this.http.post(baseURL + "/" + this.table + "/callWaiter", this.timestamp);
+    this.getCallID().subscribe((data: number) => this.callId = data);
   }
 
+  getStatus(): Observable<any>{
+    return this.http.get<String>(baseURL + "/" + this.table + "/getCallStatus");
+  }
+
+  getCallID(): Observable<any>{
+    return this.http.get<number>(baseURL + "/" + this.table + "/getCallID");
+  }
 }
