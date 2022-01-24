e1b509cc9452d1a00859a5f1e6daa10cfe1bc939
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-01-24 08:34:39 +0100

    Fixed Bug

--- src/app/model/ordereditems.ts
+++ src/app/model/ordereditems.ts
@@ -5,14 +5,16 @@ export class OrderedItems{
     status!: string;
     orderid!: number;
     orderdate!: string;
+    title!:string
 
-    constructor(ordereditemsid: number, itemid: number,quantity: number, status:string, orderid: number, orderdate: string){
+    constructor(ordereditemsid: number, itemid: number,quantity: number, status:string, orderid: number, orderdate: string, title:string){
         this.ordereditemsid = ordereditemsid;
         this.itemid = itemid;
         this.quantity  =quantity;
         this.status = status;
         this.orderid;
         this.orderdate = orderdate;
+        this.title = title;
 
     }
 }
