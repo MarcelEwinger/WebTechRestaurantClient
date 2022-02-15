887dbe11358d1616a99e51142744a73a9634e747
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-02-15 18:42:16 +0100

    Added Comments again

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -9,8 +9,6 @@ import { DbService } from '../shared/db.service';
 var placedOrder: boolean = false;
 
 
-
-
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -21,12 +19,18 @@ export class OrderComponent implements OnInit {
   panelOpenState = true;
   order: Order[] = [];
 
-  
+  /**
+   * 
+   * @param dbService Definiert DBService, um auf die Methoden der Klasse zugreifen zu können.
+   * @param sessionStorage Definiert SessionStorage, um auf die Methoden der Klasse zugreifen zu können.
+   * @param snackBar Definiert MatSnackBar, um auf die Methoden der Klasse zugreifen zu können.
+   * 
+   * Fragt die Bestellung ab und prüft, ob das Payment schon durchgeführt wurde.
+   */
   constructor(private dbService: DbService, private sessionStorage: SessionStorageService, private snackBar: MatSnackBar) {
     this.getOrder();
     if(this.dbService.orderStarted === false){
       this.displaySnackbar("Payment was not executed", 3000);
-
     }else{
       this.getOrder();
     }
@@ -38,10 +42,22 @@ export class OrderComponent implements OnInit {
    
   }
 
+  /**
+   * 
+   * @returns Gibt die Bestellung über DBService zurück.
+   */
   getOrder(){
     this.order = this.dbService.getOrder();
     return this.order;
   }
+
+  /**
+   * 
+   * @param text Übergabeparamter: String
+   * @param length Übergabeparameter: Number
+   * 
+   * Öffnet die MatSnackBar
+   */
   displaySnackbar(text: string, length: number){
     this.snackBar.open(text, '', {
       duration: length
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -24,22 +24,34 @@ export class ProductComponent {
   categories!: Category[]; 
   
   
-  
+  /**
+   * Emitters werden definiert.
+   */
   @Output() likeEmitter: EventEmitter<Product> = new EventEmitter();
   @Output() dislikeEmitter: EventEmitter<Product> =  new EventEmitter();
   @Output() shoppingCartEmitter: EventEmitter<Product> =  new EventEmitter();
 
+  /**
+   * 
+   * @param dialog Definiert MatDialog, um auf die Methoden der Klasse zugreifen zu können.
+   * @param dbService Definiert DBService, um auf die Methoden der Klasse zugreifen zu können.
+   */
   constructor(public dialog: MatDialog,  private dbService: DbService){
     setTimeout(()=>{
       this.checkStatus();
     },50);
   }
 
+  /**
+   * Methode, mit der der Dialog des Product abgerufen wird.
+   */
   openDialog() {
     this.dialog.open(DialogProductComponent, {data: {title: this.product.title, description: this.product.description, allergens: this.product.allergene}});
   }
 
-
+/**
+ * Erhöht die Likes des gelikeden Products.
+ */
   incrementLikes(){
     this.likeEmitter.emit(this.product);
     if(this.product.likes != undefined){
@@ -54,7 +66,9 @@ export class ProductComponent {
     this.rated = true;
   }
 
-
+/**
+ * Erhöht die Dislikes des gelikeden Products.
+ */
   incrementDislikes(){
     this.dislikeEmitter.emit(this.product);
     if(this.product.dislikes != undefined){
@@ -69,27 +83,32 @@ export class ProductComponent {
     
     this.rated = true;
   }
+
+  /**
+   * Fügt mittels Emitter das Produkt zum Warenkorb hinzu.
+   */
   addProduct(){
     this.shoppingCartEmitter.emit(this.product);
-     
-    
   }
+
+  /**
+   * 
+   * @param likes Anzahl Likes
+   * @param dislikes Anzahl Dislikes
+   * @returns Gibt die Anzahl der Sterne aufgrund der Likes und Dislikes zurück.
+   */
   calcStarRatio(likes: number, dislikes: number){
     return (likes/(likes+dislikes)).toFixed(2);
   }
 
+  /**
+   * Prüft, ob eine Produkt verfügbar ist.
+   */
   checkStatus(){
       if(this.product.status === "available"){
         this.notAvailable = false;
       }else{
         this.notAvailable = true;
-      }
-    
+      }   
   }
- 
-
 }
-
-
-
-
